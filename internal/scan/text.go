c7833d14e6725949ac510f470b16ca17b19434f7
Keith Marshall <clarityoverclever@protonmail.com>
2025-12-31 11:17:29 -0800

    fix (scanner): adjust buffer to scan lines tokenized to < 10kb

--- internal/scan/text.go
+++ internal/scan/text.go
@@ -26,8 +26,31 @@ func (e *TextExtractor) Extract(ctx context.Context, r io.Reader) ([]string, err
 	var lines []string
 	scanner := bufio.NewScanner(r)
 
+	// bufio.Scanner has a maximum line length of 64kb per token
+	// this will limit the line length the scanner will read to
+	// prevent errors on lines which exceed 64kb.
+
+	const maxLineLength = 10 * 1024 // 10kb
+	buf := make([]byte, maxLineLength)
+	scanner.Buffer(buf, maxLineLength)
+
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		line := scanner.Text()
+
+		if len(line) < maxLineLength {
+			lines = append(lines, line)
+		}
+
+		// ignore "token too long" errors
+		if err := scanner.Err(); err != nil {
+			if strings.Contains(err.Error(), "token too long") {
+				return lines, nil
+			}
+
+			// return other errors
+			return lines, scanner.Err()
+		}
 	}
-	return lines, scanner.Err()
+
+	return lines, nil
 }
